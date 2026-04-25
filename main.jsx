08f395ac142e11de09b6c0524c52d1ba90a4085c
saad1406vip-eng <saad1406vip@gmail.com>
2026-04-25 19:41:09 +0300

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SPLApp from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SPLApp />
-  </React.StrictMode>
-);
