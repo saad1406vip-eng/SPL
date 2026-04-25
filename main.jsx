cb3ec96913736828d80dd1cb60de4647449a9e4f
saad1406vip-eng <saad1406vip@gmail.com>
2026-04-25 19:19:23 +0300

    Initialize main.jsx with React and SPLApp

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SPLApp from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SPLApp />
+  </React.StrictMode>
+);
